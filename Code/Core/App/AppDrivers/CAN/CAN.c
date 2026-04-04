d3142b9927c81d9d5d46938330b2790f8b4ad173
Reed Basangan <michael.basangan@student.uts.edu.au>
2026-04-04 13:58:12 +1100

    finished framework for CAN driver, normal transmit function and a recive function that runs of interupt

--- Code/Core/App/AppDrivers/CAN/CAN.c
+++ Code/Core/App/AppDrivers/CAN/CAN.c
@@ -3,6 +3,7 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_can.h"
 #include "stm32f4xx_hal_def.h"
+#include <stdint.h>
 
 
 
@@ -66,8 +67,65 @@ HAL_StatusTypeDef Can_init() {
         return HAL_ERROR;
     }
 
+    result =  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+
     return result;
 
 
+
+}
+
+HAL_StatusTypeDef can_transmit(uint8_t nodeID,  uint8_t msgType, uint8_t *payload, uint8_t length) {
+
+    CAN_TxHeaderTypeDef canTX1;
+    uint32_t txMailbox;
+
+    uint32_t canID = CAN_ID(nodeID, msgType); 
+
+    canTX1.StdId = canID;
+    canTX1.IDE = CAN_ID_STD;
+    canTX1.RTR = CAN_RTR_DATA;
+    canTX1.DLC = length;
+    canTX1.TransmitGlobalTime = DISABLE;
+
+
+    HAL_StatusTypeDef result = HAL_CAN_AddTxMessage(&hcan1, &canTX1, payload, &txMailbox);
+
+    return result;
+
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+
+    CAN_RxHeaderTypeDef canRX1;
+
+    uint8_t rxData[8];
+    
+    HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &canRX1, rxData);
+
+    uint8_t nodeID = canRX1.StdId >> 7;
+    uint8_t msgType = canRX1.StdId  & 0x7f;
+
+    switch (nodeID) {
+        
+        case KESTREL :
+            switch (msgType) {
+            
+            }
+            break;
+
+        case RAVEN :
+            switch (msgType) {
+            
+            }
+            break;
+
+        case HUGINN :
+            switch (msgType) {
+            
+            }
+            break;
+    
+    }
 
 }
